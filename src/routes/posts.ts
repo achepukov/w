86041aeb0c0935f3d280c5c7e528aab9cbfe5ba9
Alexander Chepukov <a.chepukov@gmail.com>
2021-07-15 09:47:32 +0300

    create

--- src/routes/posts.ts
+++ src/routes/posts.ts
@@ -6,9 +6,16 @@ router.get('/', (req, res, next) => {
   res.send('posts!')
 });
 
-router.post('/', async (req, res, next) => {
-  const post = await Post.upsert(req.body)
-  res.send(post);
+router.post('/', (req, res, next) => {
+  const post = Post.create(req.body)
+    .then(createdPost => {
+      res.status(201);
+      res.send(createdPost);
+    })
+    .catch(err => {
+      res.status(400);
+      res.send(err);
+    })
 });
 
 export default router;
